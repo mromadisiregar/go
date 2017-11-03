a83eda091f8b5d20a6ae114f7bb1651b5818b5cd
zam <mromadisiregar@gmail.com>
2017-11-04 04:19:39 +0700

    Add zamprox On God Lang

---
+++ zamprox.go
@@ -0,0 +1,37 @@
+// Trying socket programming with go
+// Little code but huge binary
+// hemm... :|
+// zamprox on Go
+package main 
+
+import (
+	"net"	
+	"fmt"
+	"io"
+	"os"
+)
+
+// Main function
+func main() {
+	var remoteProxyAddr = os.Args[1]
+	var remoteProxyPort = os.Args[2]
+	// Port
+	var localPort = ":8888"
+	// Create sockServer
+	sockServer, _ := net.Listen("tcp", localPort)
+	fmt.Printf("Listen pada port %s\n", localPort)
+	// Create daemon
+	for {
+		sockClient, _ := sockServer.Accept()
+		sockProxy, _ := net.Dial("tcp", remoteProxyAddr + ":" + remoteProxyPort)
+		go handleCon(sockClient, sockProxy)
+		go handleCon(sockProxy, sockClient)
+	}
+	sockServer.Close()
+}
+
+func handleCon(c net.Conn, p net.Conn) {
+	defer c.Close()
+	defer p.Close()
+	io.Copy(c, p)
+}
